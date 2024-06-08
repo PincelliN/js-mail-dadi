6979b5f22f8e56bb5c8e98a7398fb31d8dad6fd2
Nicolò Pincelli <pincelli.n@gmail.com>
2024-06-08 23:28:19 +0200

    Add all style

--- js/main.js
+++ js/main.js
@@ -1,24 +1,62 @@
 /* Mail :email:
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.*/
- 
+
 //creo un array di email concesse
-const EmailOk = ["carlo@gmail","marco@gmail","paolo@gmail","giorgio@gmail","anna@gmail","otto@gmail","gino@gmail","gina@gmail","pino@gmail","pina@gmail","me@gmail","te@gmail","ti@gmail"];
-// creo la costate legata al bottone
-const Button =document.querySelector("button");
+const EmailOk = [
+  "carlo@gmail",
+  "marco@gmail",
+  "paolo@gmail",
+  "giorgio@gmail",
+  "anna@gmail",
+  "otto@gmail",
+  "gino@gmail",
+  "gina@gmail",
+  "pino@gmail",
+  "pina@gmail",
+  "me@gmail",
+  "te@gmail",
+  "ti@gmail",
+];
+// creo la costante legata al bottone
+const Button = document.querySelector("button");
+
+let Email = "<ul>";
+for (let i = 0; i < EmailOk.length; i++) {
+  Email += "<li>" + EmailOk[i] + "</li>";
+}
+Email += "</ul>";
+document.getElementById("CheckEmail").innerHTML = Email;
+
+
+let Suggerimenti = document.querySelector("span")
+Suggerimenti.addEventListener("mouseover",function () {
+  document.getElementById("CheckEmail").classList.remove("d-none");
+  }) 
+   Suggerimenti.addEventListener("mouseout",function () {
+   document.getElementById("CheckEmail").classList.add("d-none");
+})
+
+
 
 // creo un evento
-Button.addEventListener("click",function (event) {
+Button.addEventListener("click", function (event) {
   //alla caratteristica evento aggiungo preventDefoult per prevenire il reflesh automatico dovuto hai botton dentro hai form
   event.preventDefault();
-    //leggo il valore del input
-    let UserEmail =document.getElementById("email").value;
-      // confronto i valori che mi servono 
-    let Result = EmailOk.includes(UserEmail);
-    console.log(Result);
-    if(Result==true){
-        document.getElementById("result").innerHTML="<div>Ok<\div>";
-    }else{
-       document.getElementById("result").innerHTML = "<div>No<div>"; 
-    }
-    }
-)
+  //leggo il valore del input
+  let UserEmail = document.getElementById("email").value;
+  // confronto i valori che mi servono
+  let Result = EmailOk.includes(UserEmail);
+  console.log(Result);
+  if (Result == true) {
+    let Body = document.getElementById("Background");
+    Body.style.backgroundImage = "url('img/Mines_of_MoriaART1.webp')";
+    Body.innerHTML="Benvenuti a Moira";
+    Body.classList.add("text-center", "h1","text-white");
+  } else {
+    let Body = document.getElementById("Background");
+    Body.style.backgroundImage =
+      "url('img/bestiario_signore_degli_anelli_monster_movie_balrog_gandalf.webp')";
+      Body.innerHTML = "Tu Non Puoi Passare!!";
+      Body.classList.add("text-center","h1",)
+       
+}});
