900f944d00c1ee143424f68c250d56a2c5799f4b
Nicolò Pincelli <pincelli.n@gmail.com>
2024-06-08 10:47:46 +0200

    Fix email envent.preventDefoult. Fix random dice

--- Dadi/js/main.js
+++ Dadi/js/main.js
@@ -1,10 +1,14 @@
-let User=[1,2,3,4,5,6];
-let Pc = [1,2,3,4,5,6]; 
 
-
-let UserResult = Math.floor(Math.random()*User.length);
+//Creo un array con dentro le faccie di un dado
+let User=[1,2,3,4,5,6]; 
+/*Creo un variabile utilizzando l'array appena scritto 
+cancolo un valore indice attraverso la seguente somma
+Math floor = seve per arrotondare il numero ottenuto per difetto
+Math random produce un numero variabile tra 0 e 1 escudendo l'uno
+array.length mi dice quanto e lungo il mio array in questo caso 6  */
+let UserResult = User[ Math.floor(Math.random()*User.length)];
 console.log(UserResult);
-let PcResult = Math.floor(Math.random() * Pc.length);
+let PcResult = User[Math.floor(Math.random() * User.length)];
 console.log(PcResult);
 if(UserResult>PcResult){
     console.log("you win");
--- js/main.js
+++ js/main.js
@@ -5,17 +5,20 @@ Chiedi all’utente la sua email, controlla che sia nella lista di chi può acce
 const EmailOk = ["carlo@gmail","marco@gmail","paolo@gmail","giorgio@gmail","anna@gmail","otto@gmail","gino@gmail","gina@gmail","pino@gmail","pina@gmail","me@gmail","te@gmail","ti@gmail"];
 // creo la costate legata al bottone
 const Button =document.querySelector("button");
+
 // creo un evento
-Button.addEventListener("click",function () {
+Button.addEventListener("click",function (event) {
+  //alla caratteristica evento aggiungo preventDefoult per prevenire il reflesh automatico dovuto hai botton dentro hai form
+  event.preventDefault();
     //leggo il valore del input
     let UserEmail =document.getElementById("email").value;
       // confronto i valori che mi servono 
     let Result = EmailOk.includes(UserEmail);
     console.log(Result);
     if(Result==true){
-        document.getElementById("true").classList.remove("none");
+        document.getElementById("result").innerHTML="<div>Ok<\div>";
     }else{
-       document.getElementById("false").classList.remove("none"); 
+       document.getElementById("result").innerHTML = "<div>No<div>"; 
     }
     }
 )
